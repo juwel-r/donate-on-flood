c3a7bbeafccc9f10b51804a13a1a3d792cb06ac9
juwel-r <juwel.jem@gmail.com>
2024-09-23 00:42:12 +0600

    nav added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          custom: ['"Your Custom Font"', 'sans-serif'], // Custom font family
+        },
+        colors: {
+          customColor: '#123456',  // Custom background or text color
+          anotherColor: '#abcdef', // Another custom color
+        },
+      },
+    },
+    plugins: [],
+  };
+  
